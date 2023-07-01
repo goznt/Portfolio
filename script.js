8a6d612da1d110c5995f8c9a5cfeabdb217bc3f7
Rayy <96776929+goznt@users.noreply.github.com>
2023-07-01 22:54:04 +0800

    Update Portfolio

--- script.js
+++ script.js
@@ -25,3 +25,5 @@ ScrollReveal().reveal('.home-bio h1 .about-resume', { origin: 'left' });
 ScrollReveal().reveal('.home-bio p', { origin: 'right'});
 ScrollReveal().reveal('.home-bio, heading', { origin: 'top'});
 ScrollReveal().reveal('.profile-pic, .about-items, .skills-items, .projects-items, .form', { origin: 'bottom'});
+
+
